8fe13484e040b0296661c78448bbcc48bd46cb6a
Vadim <vadimyanchenko64@gmail.com>
2024-10-24 20:23:20 +0400

    fix: перенос папок

--- src/main/java/backend/academy/controller/dto/CellDto.java
+++
@@ -1,8 +0,0 @@
-package backend.academy.controller.dto;
-
-import backend.academy.maze.cell.Coordinate;
-import com.fasterxml.jackson.annotation.JsonAnyGetter;
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public record CellDto(@JsonProperty Coordinate coordinate, @JsonProperty boolean isWall) {
-}
--- src/main/java/backend/academy/controller/dto/LabyrinthDto.java
+++
@@ -1,7 +0,0 @@
-package backend.academy.controller.dto;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public record LabyrinthDto(@JsonProperty int height, @JsonProperty int width,
-                           @JsonProperty String generatorName) {
-}
